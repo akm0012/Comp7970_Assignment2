50f3fe7102227dff6a649556efb160967b705901
akm0012 <akm0012@auburn.edu>
2014-06-11 20:20:26 -0500

    Initial commit of iOS code.

---
+++ Assignment 2/Assignment 2/akm0012_ViewController.h
@@ -0,0 +1,13 @@
+//
+//  akm0012_ViewController.h
+//  Assignment 2
+//
+//  Created by Andrew Marshall on 6/11/14.
+//  Copyright (c) 2014 Comp_7970_akm0012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface akm0012_ViewController : UIViewController
+
+@end
---
+++ Assignment 2/Assignment 2/akm0012_ViewController.m
@@ -0,0 +1,29 @@
+//
+//  akm0012_ViewController.m
+//  Assignment 2
+//
+//  Created by Andrew Marshall on 6/11/14.
+//  Copyright (c) 2014 Comp_7970_akm0012. All rights reserved.
+//
+
+#import "akm0012_ViewController.h"
+
+@interface akm0012_ViewController ()
+
+@end
+
+@implementation akm0012_ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
