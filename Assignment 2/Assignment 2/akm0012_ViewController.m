3cf1fe174e17181dfa2754b385117e59b427d0ab
akm0012 <akm0012@auburn.edu>
2014-06-13 14:48:29 -0500

    Buttons and labels are wired up.  Layout method stubs added.

--- Assignment 2/Assignment 2/akm0012_ViewController.m
+++ Assignment 2/Assignment 2/akm0012_ViewController.m
@@ -9,15 +9,36 @@
 #import "akm0012_ViewController.h"
 
 @interface akm0012_ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *title_label;
+@property (weak, nonatomic) IBOutlet UILabel *loan_amt_label;
+@property (weak, nonatomic) IBOutlet UITextField *loan_amt_textField;
+@property (weak, nonatomic) IBOutlet UILabel *num_payment_label;
+@property (weak, nonatomic) IBOutlet UITextField *num_payment_textField;
+@property (weak, nonatomic) IBOutlet UILabel *interest_rate_label;
+@property (weak, nonatomic) IBOutlet UITextField *interest_rate_textField;
+@property (weak, nonatomic) IBOutlet UILabel *ballon_pmt_label;
+@property (weak, nonatomic) IBOutlet UITextField *ballon_pmt_textField;
+@property (weak, nonatomic) IBOutlet UIButton *calculate_button;
+@property (weak, nonatomic) IBOutlet UILabel *payment_amount_label;
+@property (weak, nonatomic) IBOutlet UILabel *payment_display_label;
+- (IBAction)calculate_button_pressed:(UIButton *)sender;
 
 @end
 
 @implementation akm0012_ViewController
 
+// Constants
+static const CGFloat buttonHeight = 40;
+static const CGFloat buttonWidth = 120;
+static const CGFloat spacing = 20;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIApplication *app = [UIApplication sharedApplication];
+    UIInterfaceOrientation current_orientation = app.statusBarOrientation;
+    [self doLayoutForOrientation:current_orientation];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +46,37 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+    
+    [self doLayoutForOrientation:toInterfaceOrientation];
+}
+
+- (IBAction)calculate_button_pressed:(UIButton *)sender {
+}
+
+- (void)doLayoutForOrientation:(UIInterfaceOrientation) orientation {
+    
+    if (UIInterfaceOrientationIsPortrait(orientation)) {
+        [self layoutPortrait];
+    }
+    
+    else {
+        [self layoutLandscape];
+    }
+    
+}
+
+- (void) layoutPortrait {
+    
+    
+}
+
+- (void) layoutLandscape {
+    
+    
+}
+
+
 
 @end
